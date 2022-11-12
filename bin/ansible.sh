7324adfd74be55673e4091ac53481facf28ecc1c
Jakob Boghdady <58260820+jakoberpf@users.noreply.github.com>
2022-11-12 02:33:15 +0100

    update ansible and tooling scripts

--- bin/ansible.sh
+++ bin/ansible.sh
@@ -2,11 +2,11 @@
 GIT_ROOT=$(git rev-parse --show-toplevel)
 cd $GIT_ROOT
 
+# Make sure virtual environment is activated
+source $GIT_ROOT/.venv/ansible/bin/activate
+
 # Run terraform apply
 cd $GIT_ROOT/ansible
-
-# Make sure virtual environment is activated
-source .venv/bin/activate
 
 # Run ansible
 ansible-playbook plays/main.yaml
--- bin/tooling.sh
+++ bin/tooling.sh
@@ -1,30 +1,21 @@
 #!/usr/bin/env bash
 GIT_ROOT=$(git rev-parse --show-toplevel)
 
-cd $GIT_ROOT/terraform
-
 terraform_version="1.1.5"
-
-tfenv install $terraform_version
-tfenv use $terraform_version
-
-m1-terraform-provider-helper install hashicorp/template -v v2.2.0
-
-cd $GIT_ROOT/ansible
-
 python_version="3.9.6"
 ansible_version="4.8.0"
 
-# Create virtual environment
-python -m venv .venv
+python -m venv .venv/terraform
+source .venv/terraform/bin/activate
+python -m pip install --upgrade pip
+python -m pip install --upgrade checkov
+python -m pip install --upgrade ansible==6.5.0
 
-# Activate virtual environment
-source .venv/bin/activate
-
-# Install ansible
-pip3 install --upgrade pip
-pip3 install ansible==$ansible_version
-pip3 install jmespath dnspython netaddr
-
-# Get roles from galaxy
+python -m venv .venv/ansible
+source .venv/ansible/bin/activate
+python -m pip install --upgrade pip
+python -m pip install --upgrade ansible==$ansible_version
+python -m pip install --upgrade jmespath dnspython netaddr
 ansible-galaxy install systemli.letsencrypt
+
+# m1-terraform-provider-helper install hashicorp/template -v v2.2.0
