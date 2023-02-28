c6feabb395df22be800c2a73ed6bd1f43d3e50f5
Jakob Boghdady <58260820+jakoberpf@users.noreply.github.com>
2023-02-28 16:37:36 +0100

    add tooling script

--- bin/tooling.sh
+++ bin/tooling.sh
@@ -5,17 +5,15 @@ terraform_version="1.1.5"
 python_version="3.9.6"
 ansible_version="4.8.0"
 
-python -m venv .venv/terraform
+python3 -m venv .venv/terraform
+python3 -m venv .venv/ansible
+
 source .venv/terraform/bin/activate
 python -m pip install --upgrade pip
 python -m pip install --upgrade checkov
-python -m pip install --upgrade ansible==6.5.0
 
-python -m venv .venv/ansible
 source .venv/ansible/bin/activate
 python -m pip install --upgrade pip
 python -m pip install --upgrade ansible==$ansible_version
 python -m pip install --upgrade jmespath dnspython netaddr
 ansible-galaxy install systemli.letsencrypt
-
-# m1-terraform-provider-helper install hashicorp/template -v v2.2.0
